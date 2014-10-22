f90c7f44e032b76a0a06c30c70ab7b4503afc573
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2014-10-22 20:09:42 +0300

    Initial release

---
+++ README.rst
@@ -0,0 +1,27 @@
+.. redactor-plugins-lineheight
+.. README.rst
+
+A redactor-plugins-lineheight documentation
+===========================================
+
+    *redactor-plugins-lineheight is a plugin for Redactor Text Editor (http://imperavi.com/redactor/) to set selected text line height*
+
+.. contents::
+
+Installation
+------------
+* Obtain your copy of source code from git repository: ``git clone https://github.com/vint21h/redactor-plugins-lineheight.git``. Or download latest release from https://github.com/vint21h/redactor-plugins-lineheight/tags.
+* Add ``Font Awesome`` css file and ``redactor-plugins-lineheight`` js file to your page.
+* Add ``lineheight`` plugin to redactor options plugins list.
+* For more complete info see ``http://imperavi.com/redactor/docs/``.
+
+
+Licensing
+---------
+redactor-plugins-lineheight uses the MIT license. Please check the MIT-LICENSE file for more details.
+
+Contacts
+--------
+**Project website**: https://github.com/vint21h/redactor-plugins-lineheight
+
+**Author**: Alexei Andrushievich <vint21h@vint21h.pp.ua>
---
+++ js/lineheight.js
@@ -0,0 +1,34 @@
+// redactor-plugins-lineheight
+// js/lineheight.js
+
+if (!RedactorPlugins) var RedactorPlugins = {};
+
+RedactorPlugins.lineheight = function()
+{
+    return {
+        init: function()
+        {
+            var heights = [1, 1.25, 1.5, 2, 2.5, 3];
+            var that = this;
+            var dropdown = {};
+
+            $.each(heights, function(i, s)
+            {
+                dropdown['s' + i] = { title: s + 'em', func: function() { that.lineheight.set(s); } };
+            });
+
+            dropdown.remove = { title: 'Remove Line Height', func: that.lineheight.reset };
+            var button = this.button.add('lineheight', 'Change Line Height');
+            this.button.setAwesome('lineheight', 'fa-arrows-v');
+            this.button.addDropdown(button, dropdown);
+        },
+        set: function(height)
+        {
+            this.inline.format('span', 'style', 'line-height: ' + height + 'em;');
+        },
+        reset: function()
+        {
+            this.inline.removeStyleRule('line-height');
+        }
+    };
+};
