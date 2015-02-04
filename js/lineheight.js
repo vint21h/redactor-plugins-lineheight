ef1cbb20879ccbba97b8a13ed486f662000d0f80
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2015-02-04 04:28:13 +0200

    code style fixes

--- js/lineheight.js
+++ js/lineheight.js
@@ -8,16 +8,23 @@ RedactorPlugins.lineheight = function()
     return {
         init: function()
         {
-            var heights = [1, 1.25, 1.5, 2, 2.5, 3];
-            var that = this;
-            var dropdown = {};
+            var heights = [1, 1.25, 1.5, 2, 2.5, 3],
+                that = this,
+                dropdown = {};
 
             $.each(heights, function(i, s)
             {
-                dropdown['s' + i] = { title: s + 'em', func: function() { that.lineheight.set(s); } };
+                dropdown['s' + i] = {
+                    title: s + 'em', func: function() {
+                        that.lineheight.set(s);
+                    }
+                };
             });
 
-            dropdown.remove = { title: 'Remove Line Height', func: that.lineheight.reset };
+            dropdown.remove = {
+                title: 'Remove Line Height',
+                func: that.lineheight.reset
+            };
             var button = this.button.add('lineheight', 'Change Line Height');
             this.button.setAwesome('lineheight', 'fa-arrows-v');
             this.button.addDropdown(button, dropdown);
